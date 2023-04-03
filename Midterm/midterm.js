356083044e2594287fe0299a051bdd6b7f382a41
milesklass <miles.klass@gmail.com>
2023-04-03 18:08:26 -0400

    update

--- Midterm/midterm.js
+++ Midterm/midterm.js
@@ -1,36 +1,44 @@
-<footer>
-  <p id="breakpoint-message"></p>
-</footer>
-
-<script>
-  // get the current width of the window
-  const windowWidth = window.innerWidth;
-
-  // get the message element in the footer
-  const breakpointMessage = document.getElementById('breakpoint-message');
-
-  // set the message based on the current breakpoint
-  if (windowWidth < 576) {
-    breakpointMessage.textContent = 'You are currently on a mobile device';
-  } else if (windowWidth < 768) {
-    breakpointMessage.textContent = 'You are on a tablet device';
-  } else {
-    breakpointMessage.textContent = 'Desktop devices are the best to view my website';
-  }
-</script>
-
-
-
- <p id="greeting-message">Welcome!</p>
-const greetingMsg = document.getElementById("greeting-message");
+// Get current hour
 const currentHour = new Date().getHours();
 
+// Greet the user based on the time of the day
+let greeting;
 if (currentHour < 12) {
-  greetingMsg.textContent = "Good morning!";
+  greeting = 'Good morning';
 } else if (currentHour < 16) {
-  greetingMsg.textContent = "Good afternoon!";
+  greeting = 'Good afternoon';
 } else if (currentHour < 20) {
-  greetingMsg.textContent = "Good evening!";
+  greeting = 'Good evening';
 } else {
-  greetingMsg.textContent = "Good night!";
+  greeting = 'Good night';
 }
+
+// Display the greeting message
+const greetingElement = document.getElementById('greeting');
+greetingElement.innerText = greeting;
+
+  // Define breakpoints for mobile, tablet, and desktop devices
+const MOBILE_BREAKPOINT = 567;
+const TABLET_BREAKPOINT = 768;
+const DESKTOP_BREAKPOINT = 1024;
+
+// Get the width of the window
+const windowWidth = window.innerWidth;
+
+// Define message based on screen size
+let message;
+if (windowWidth < MOBILE_BREAKPOINT) {
+  message = "You are on a mobile device";
+} else if (windowWidth < TABLET_BREAKPOINT) {
+  message = "You are on a tablet device";
+} else if (windowWidth < DESKTOP_BREAKPOINT) {
+  message = "You are on a desktop device";
+} else {
+  message = "You are on a desktop device";
+}
+
+// Display message in the footer of the page
+const footer = document.querySelector("footer");
+const messageElement = document.createElement("h3");
+messageElement.textContent = message;
+footer.appendChild(messageElement);
